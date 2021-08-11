cb57f6292978e5402d7688f37a1340cb2cc6b9cf
anzhihe <anzhihe1218@gmail.com>
2021-08-11 08:44:50 +0800

    modified:   string_reversed_sort.py

--- python/practise/learn-python/python_basic/string_reversed_sort.py
+++ python/practise/learn-python/python_basic/string_reversed_sort.py
@@ -22,3 +22,21 @@ print(iterator)         # <reversed object at 0x10d3d2e20>
 print(list(iterator))   # ['5', '4', '3', '2', '1']
 
 print(s)    # 12345
+
+
+"""二、字符串的排序"""
+
+s = 'DBeFac'
+print(sorted(s))                    # ['B', 'D', 'F', 'a', 'c', 'e']
+print(sorted(s, reverse = True))    # ['e', 'c', 'a', 'F', 'D', 'B']
+print(sorted(s, key = str.lower))   # ['a', 'B', 'c', 'D', 'e', 'F']
+
+L = ['Python', 'java', 'Swift']
+print(sorted(L, key = len))         # ['java', 'Swift', 'Python']
+print(sorted(L, key = str.lower))   # ['java', 'Python', 'Swift']
+
+L = ['Python', 'java', 'Swift']
+L.sort(key = len)
+print(L)                            # ['java', 'Swift', 'Python']
+L.sort(key = str.lower)
+print(L)                            # ['java', 'Python', 'Swift']
