fede8782a0329874f1fb1dd0f4e68171bbfe5314
anzhihe <anzhihe1218@gmail.com>
2022-11-29 00:00:16 +0800

    add Python之Django篇第八季：用户权限管理

---
+++ python/practise/Python之Django篇第八季：用户权限管理/Django第八季完整项目源码-01/StudentMgr/apps/web/userweb/tests.py
@@ -0,0 +1,45 @@
+# Create your tests here.
+
+
+# 第一题
+def get_common_str(str1, str2):
+    lstr1 = len(str1)
+    lstr2 = len(str2)
+    record = [[0 for i in range(lstr2 + 1)] for j in range(lstr1 + 1)]  # 多一位
+    maxNum = 0  # 最长匹配长度
+    p = 0  # 匹配的起始位
+
+    for i in range(lstr1):
+        for j in range(lstr2):
+            if str1[i] == str2[j]:
+                # 相同则累加
+                record[i + 1][j + 1] = record[i][j] + 1
+                if record[i + 1][j + 1] > maxNum:
+                    # 获取最大匹配长度
+                    maxNum = record[i + 1][j + 1]
+                    # 记录最大匹配长度的终止位置
+                    p = i + 1
+    return str1[p - maxNum:p]
+
+if __name__ == '__main__':
+    print(get_common_str("aaddabcdee", 'ddbbabcdff'))
+
+
+
+
+
+# 编程题第二题
+def is_ugly_number(num: int):
+    while num % 2 == 0:
+        num = num / 2
+    while num % 3 == 0:
+        num = num / 3
+    while num % 5 == 0:
+        num = num / 5
+    if num == 1:
+        return True
+    else:
+        return False
+
+
+
